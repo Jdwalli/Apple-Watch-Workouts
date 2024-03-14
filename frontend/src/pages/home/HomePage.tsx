cb8dafad4d9d533d88c182b789a58df17ecede79
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-03-14 18:27:27 -0400

    Added pages

---
+++ frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const HomePage: React.FC<Props> = (props: Props) => {
+  return (
+    <div>
+      <h1> Welcome to the Home Page!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/sleep/SleepPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const SleepPage: React.FC<Props> = (props: Props) => {
+  return (
+    <div>
+      <h1> Welcome to the Sleep Page!</h1>
+    </div>
+  );
+};
+
+export default SleepPage;
