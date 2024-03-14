73c9951453274445526458362e3472d128308a27
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-03-14 18:27:39 -0400

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/home/HomePage";
+import ActivityPage from "./pages/activity/ActivityPage";
+import VitalsPage from "./pages/vitals/VitalsPage";
+import WorkoutsPage from "./pages/workouts/WorkoutsPage";
+import SleepPage from "./pages/sleep/SleepPage";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/activity" element={<ActivityPage />} />
+        <Route path="/vitals" element={<VitalsPage />} />
+        <Route path="/workouts" element={<WorkoutsPage />} />
+        <Route path="/sleep" element={<SleepPage />} />
+      </Routes>
+    </div>
   );
 }
 
