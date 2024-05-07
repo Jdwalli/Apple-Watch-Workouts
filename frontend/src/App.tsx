d5428f66f1676cc6c9ae3c6bf35893927bae74bf
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-05-07 18:39:50 -0400

    Removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
-//Layout
-import Layout from "./components/shared/Layout";
 
 // Pages
 import HomePage from "./pages/home/HomePage";
-import ActivityPage from "./pages/activity/ActivityPage";
-import VitalsPage from "./pages/vitals/VitalsPage";
 import WorkoutsPage from "./pages/workouts/WorkoutsPage";
-import SleepPage from "./pages/sleep/SleepPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/activity" element={<ActivityPage />} />
-        <Route path="/vitals" element={<VitalsPage />} />
-        <Route path="/workouts" element={<WorkoutsPage />} />
-        <Route path="/sleep" element={<SleepPage />} />
-      </Route>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/workouts" element={<WorkoutsPage />} />
     </Routes>
   );
 }
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,40 +1,12 @@
 import React from "react";
-import { HealthRecordApiClient } from "../../clients/health_record_client";
-import { ReturnedActivityRecords, WorkoutBreakdownData } from "../../types/client_types";
-import RecordsStatisticsGrid from "./RecordsStatisticsGrid";
-import WorkoutsBreakdownPieChart from "../../components/charts/WorkoutsBreakdownPieChart";
-
-// Pie Chart (Total Workouts) 
-// Vitals
-// Highest HR, Lowest HR, Heart Rate Variabilitu, Wallimg Hr, Average, Cardio Fitness, ECGs taken, Notifications given, Resp Rate, Blood 02
-
+import TopHeader from "../../components/navigation/TopHeader";
 
 interface Props {}
 
 const HomePage: React.FC<Props> = (props: Props) => {
-  const healthRecordClient = new HealthRecordApiClient();
-  const [recordStatisticsData, setRecordStatisticsData] = React.useState<ReturnedActivityRecords[]>([])
-  const [totalWorkouts, setTotalWorkouts] = React.useState<number>(0);
-  const [workoutBreakdownData, setWorkoutBreakdownData] = React.useState<WorkoutBreakdownData[]>([]);
-
-  React.useEffect(() => {
-    healthRecordClient.getUserRecord().then((data) => {
-      setRecordStatisticsData(data.activityRecords);
-      setTotalWorkouts(data.workoutRecords.totalWorkouts);
-      setWorkoutBreakdownData(data.workoutRecords.workoutBreakdown);
-    });
-
-  }, []);
-
   return (
-    <div className="flex flex-col gap-4">
-      <RecordsStatisticsGrid recordsData={recordStatisticsData} />
-      <div className="flex flex-row gap-4 w-full">
-        {/* <ThirtyDayActivityLineChart /> */}
-        <WorkoutsBreakdownPieChart totalRecorded={totalWorkouts} data={workoutBreakdownData} />
-      </div>
-      <div className="flex flex-row gap-4 w-full"></div>
-      <div className="flex flex-row gap-4 w-full"></div>
+    <div>
+      <TopHeader />
     </div>
   );
 };
